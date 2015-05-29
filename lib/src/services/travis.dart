86e629feafdb058d39e6a6a3afa9c582a219c967
Kevin Moore <kevmoo@google.com>
2015-05-28 20:59:16 -0700

    Provide a reasonable value for service name on Travis
    Closes #34

--- lib/src/services/travis.dart
+++ lib/src/services/travis.dart
@@ -6,3 +6,22 @@ library coveralls_dart.src.services.travis;
 /// If none exists, return `null`.
 String getBranch(Map<String, String> environment) =>
     environment["TRAVIS_BRANCH"];
+
+String getServiceName(Map<String, String> environment) {
+  if (!environment.containsKey('TRAVIS')) return null;
+
+  var repoSlug = environment['TRAVIS_REPO_SLUG'];
+  if (repoSlug == null) return null;
+
+  var ref = getBranch(environment);
+
+  if (ref == null) {
+    ref = environment['TRAVIS_COMMIT'];
+  }
+
+  if (ref != null) {
+    return 'travis: $repoSlug @ $ref';
+  }
+
+  return 'travis: $repoSlug';
+}
